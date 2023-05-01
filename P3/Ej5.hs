92244d086ccb4a59d0f8bdd71950d455f64a345b
Juanse512 <juanseval1@gmail.com>
2023-05-01 16:17:00 -0300

    Finished p3

--- P3/Ej5.hs
+++ P3/Ej5.hs
@@ -11,15 +11,19 @@ eval (Div x1 x2) = (eval x1) `div` (eval x2)
 
 parseEval :: Char -> Exp -> Exp -> Exp
 parseEval '+' x1 x2 = Add x1 x2
-parseEval '-' x1 x2 = Sub x1 x2
+parseEval '-' x1 x2 = Sub x2 x1
 parseEval '*' x1 x2 = Prod x1 x2
 parseEval '/' x1 x2 = Div x1 x2
 
 
-parseRPNAux :: [Exp] -> [String] -> Exp
-
+parseRPNAux :: [Exp] -> String -> Exp
+parseRPNAux e [x] = parseEval x (head e) (head (tail e))
+parseRPNAux e (x:xs) = if isDigit x then parseRPNAux ((Lit (digitToInt x)):e) xs else parseRPNAux ((parseEval x (head e) (head (tail e))):(tail (tail e))) xs
 
 parseRPN :: String -> Exp
-parseRPN [x] = Lit x
+parseRPN [x] = Lit (digitToInt x)
+parseRPN x = parseRPNAux [] x
 
---Unfinished
+evalRPN :: String -> Int
+evalRPN [] = 0
+evalRPN x = eval (parseRPN x)
